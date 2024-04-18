e44b3b7456e455d6d0eb8835ff5f8747eecbc095
DESKTOP-0LGSBST\M0267 <745368206@qq.com>
2024-04-18 11:56:50 +0800

    激活界面提交7

--- CPP/Zipr/UI/FileManager/AboutDialog.cpp
+++ CPP/Zipr/UI/FileManager/AboutDialog.cpp
@@ -14,6 +14,7 @@
 #include "HelpUtils.h"
 #include "LangUtils.h"
 #include "App.h"
+#include "resource.h"
 
 static const UInt32 kLangIDs[] =
 {
@@ -44,7 +45,46 @@ bool CAboutDialog::OnInit()
   LangSetDlgItems(*this, kLangIDs, ARRAY_SIZE(kLangIDs));
   SetItemText(IDT_ABOUT_VERSION, UString("Zipr " MY_VERSION_CPU));
   SetItemText(IDT_ABOUT_DATE, LLL(MY_DATE));
-  
+
+  UString fileName = fs2us(NWindows::NDLL::GetModuleDirPrefix());
+  fileName += "reg.txt";
+  HANDLE hFile = CreateFileW(
+        fileName,                 
+        GENERIC_READ,                  
+        0,                             
+        NULL,                          
+        OPEN_EXISTING,                 
+        FILE_ATTRIBUTE_NORMAL,         
+        NULL                           
+  );
+ 
+  if (hFile != INVALID_HANDLE_VALUE) 
+  {
+      DWORD dwSize = GetFileSize(hFile, NULL);
+      LPWSTR lpBuffer = new WCHAR[dwSize / sizeof(WCHAR) + 1];
+      DWORD dwRead;
+      UString activateCode;
+      UString activateDate;
+
+      if (!ReadFile(hFile, lpBuffer, dwSize, &dwRead, NULL)) {
+         
+          CloseHandle(hFile);
+          delete[] lpBuffer;
+      }
+      else {
+          lpBuffer[dwSize / sizeof(WCHAR)] = L'\0';
+          LPCWSTR lpcwszBuffer = lpBuffer;
+          activateCode = lpcwszBuffer;
+          activateDate = lpcwszBuffer;
+          SetItemText(IDT_ABOUT_INFO, LangString(IDS_ACTIVATE_SOFTWAR_ERROR3) + activateCode);
+          SetItemText(IDT_ABOUT_INFO2, LangString(IDS_ACTIVATE_SOFTWAR_ERROR4) + activateDate);
+
+          //CloseHandle(hFile);
+          delete[] lpBuffer;
+      }
+  }
+ 
+  CloseHandle(hFile); 
   LangSetWindowText(*this, IDD_ABOUT);
   NormalizePosition();
   return CModalDialog::OnInit();
--- CPP/Zipr/UI/FileManager/AboutDialogRes.h
+++ CPP/Zipr/UI/FileManager/AboutDialogRes.h
@@ -1,6 +1,7 @@
 #define IDD_ABOUT  2900
 
 #define IDT_ABOUT_INFO  2901
+#define IDT_ABOUT_INFO2  2902
 
 #define IDI_LOGO             100
 #define IDT_ABOUT_VERSION    101
